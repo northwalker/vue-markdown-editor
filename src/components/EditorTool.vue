e23640a6f9a04f0446e31a8e7a42ceb295c461be
Northwalker <northwalker28@gmail.com>
2019-03-19 01:10:17 +0800

    Add new tool feature

---
+++ src/components/EditorTool.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="editor-tool">
+    <!-- TODO -->
+  </div>
+</template>
+<script>
+// TODO
+export default {
+  name: 'EditorTool',
+  components: {},
+  props: {},
+  data () {
+    return {}
+  },
+  computed: {},
+  watch: {},
+  created () {},
+  mounted () {},
+  beforeDestroy () {},
+  updated () {},
+  methods: {}
+}
+</script>
+<style lang="scss" scoped>
+.editor-tool {
+  // TODO
+}
+</style>
